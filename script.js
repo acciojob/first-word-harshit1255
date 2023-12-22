a423225a98884d1d8bb67103dc44b7e41f1b4a37
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-22 11:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
   // your code here
+	let arr = s.split(' ');
+	let val =arr[0].length ===0?arr[1]:arr[0];
+	return val;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
